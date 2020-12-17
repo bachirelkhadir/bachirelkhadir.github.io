b2944ab89bf0ec13c69453fbc55f0481e55599fe
bachirelkhadir <me@bachir.xyz>
2020-12-17 03:38:46 -0500

    compile command as dir var

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((prog-mode . ((projectile-project-compilation-cmd  . "bundle exec jekyll serve --livereload"))))
